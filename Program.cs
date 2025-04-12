b62fb7e3277d8008c3765aaed1f8844b895187d1
Danilo Costa <dandan_costa2222@outlook.com>
2025-04-12 17:06:40 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int numeroAtual = 0, proximoNumero = 1, QtdElementos, i, soma;
+
+Console.Write("Quantidade de elementos: ");
+QtdElementos = int.Parse(Console.ReadLine()!);
+
+if (QtdElementos == 1){
+    Console.WriteLine(numeroAtual);
+}
+else{
+    for(i = 1; i <= QtdElementos; i++){
+        
+        soma = numeroAtual + proximoNumero;
+        Console.WriteLine(numeroAtual);
+        
+        numeroAtual = proximoNumero;
+        proximoNumero = soma;
+        
+        }
+}
+
